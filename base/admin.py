1398e6c42dbcb582c0c20577d1244a4a4c124d55
Prashant Adhikari <aprashant057@gmail.com>
2024-02-29 21:31:31 +0545

    added models

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(SchoolSetup)
+admin.site.register(Socials)
+admin.site.register(About)
+admin.site.register(Vision)
+admin.site.register(Mission)
+admin.site.register(MessageFrom)
+admin.site.register(TeamMember)
+admin.site.register(Testimonial)
+admin.site.register(Courses)
+admin.site.register(Faqs)
+admin.site.register(Blog)
+admin.site.register(Gallery)
+admin.site.register(Contact)
+admin.site.register(HomeContent)
+admin.site.register(PopupMessage)
+admin.site.register(Notice)
+admin.site.register(Vacancy)
