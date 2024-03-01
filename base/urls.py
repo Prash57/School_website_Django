6cceceb97526471e76e56ce7c68c5da4262bcfaa
Prashant Adhikari <aprashant057@gmail.com>
2024-03-01 17:30:41 +0545

    added dynamic routes via frontend for about, vision, mission and message_from

--- base/urls.py
+++ base/urls.py
@@ -2,16 +2,50 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # home page routes
     path('', views.home, name='home'),
     path('add-home-content/', views.addHomeContent, name='addhomecontent'),
     path('edit-home-content/<str:pk>', views.editHomeContent, name='edithomecontent'),
+    path('delete-home-content/<str:pk>', views.deleteHomeContent, name='deletehomecontent'),
+
+    # about page routes 
     path('about/', views.about, name='about'),
+
+    # about page routes for about section
+    path('add-about/', views.addAbout, name='addabout'),
+    path('edit-about/<str:pk>', views.editAbout, name='editabout'),
+    path('delete-about/<str:pk>', views.deleteAbout, name='deleteabout'),
+
+    # about page routes for Vision section
+    path('add-vision/', views.addVision, name='addvision'),
+    path('edit-vision/<str:pk>', views.editVision, name='editvision'),
+    path('delete-vision/<str:pk>', views.deleteVision, name='deletevision'),
+
+    # about page routes for Mission section
+    path('add-mission/', views.addMission, name='addmission'),
+    path('edit-mission/<str:pk>', views.editMission, name='editmission'),
+    path('delete-mission/<str:pk>', views.deleteMission, name='deletemission'),
+
+    # about page routes for Message From section
+    path('add-message-from/', views.addMessageFrom, name='addmessagefrom'),
+    path('edit-message-from/<str:pk>', views.editMessageFrom, name='editmessagefrom'),
+    path('delete-message-form/<str:pk>', views.deleteMessageFrom, name='deletemessagefrom'),
+
+    # blogs page routes 
     path('blogs/', views.blogs, name='blogs'),
     path('blog/<str:pk>', views.blogDetail, name='blog'),
+
+    # gallery page routes 
     path('gallery/', views.gallery, name='gallery'),
+
+    # contact page routes 
     path('contact/', views.contact_form, name='contact'),
     path('view-contacts/', views.view_contacts, name='contacts'),
+
+    # carrers page routes 
     path('carrers/', views.carrers, name='carrers'),
     path('carrer/<str:pk>', views.carrersDetail, name='carrer'),
+
+    # notice page routes 
     path('notice/', views.notice, name='notice'),
 ]
--- base/views.py
+++ base/views.py
@@ -64,6 +64,175 @@ def about(request):
         context = {'about': about}
     return render (request, 'about.html', context)
 
+# add about section
+def addAbout(request):
+    about = About.objects.all()
+    form = AboutForm()
+    if request.method == 'POST':
+        form = AboutForm(request.POST, request.FILES)
+        if form.is_valid():
+            about = form.save(commit=False)
+            about.save()
+
+            messages.success(request, "About content saved successfully")
+            return redirect('about')
+    context = {'about': about, 'form': form}
+    return render(request, 'base/aboutform.html', context)
+
+# edit about section
+def editAbout(request, pk):
+    about = About.objects.get(id=pk)
+    form = AboutForm(instance=about)
+    if request.method == 'POST':
+        form = AboutForm(request.POST, request.FILES, instance=about)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, "About content updated successfully")
+            return redirect('about')
+    context = {'form': form}
+    return render(request, 'base/aboutform.html', context)
+
+
+# delete about section
+def deleteAbout(request,pk):
+    about = About.objects.get(id=pk)
+    if request.method == 'POST':
+        about.delete()
+        
+        messages.success(request, 'About content deleted successfully')
+        return redirect('about')
+    context = {'obj': about}
+    return render(request, 'delete.html', context)
+
+
+# add vision section
+def addVision(request):
+    vision = Vision.objects.all()
+    form = VisionForm()
+    if request.method == 'POST':
+        form = VisionForm(request.POST, request.FILES)
+        if form.is_valid():
+            vision = form.save(commit=False)
+            vision.save()
+
+            messages.success(request, "Vision content saved successfully")
+            return redirect('about')
+    context = {'vision': vision, 'form': form}
+    return render(request, 'base/visionform.html', context)
+
+# edit vision section
+def editVision(request, pk):
+    vision = Vision.objects.get(id=pk)
+    form = VisionForm(instance=vision)
+    if request.method == 'POST':
+        form = VisionForm(request.POST, request.FILES, instance=vision)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, "Vision content updated successfully")
+            return redirect('about')
+    context = {'form': form}
+    return render(request, 'base/visionform.html', context)
+
+
+# delete vision section
+def deleteVision(request,pk):
+    vision = Vision.objects.get(id=pk)
+    if request.method == 'POST':
+        vision.delete()
+        
+        messages.success(request, 'Vision content deleted successfully')
+        return redirect('about')
+    context = {'obj': vision}
+    return render(request, 'delete.html', context)
+
+
+
+# add mission section
+def addMission(request):
+    mission = Mission.objects.all()
+    form = MissionForm()
+    if request.method == 'POST':
+        form = MissionForm(request.POST, request.FILES)
+        if form.is_valid():
+            mission = form.save(commit=False)
+            mission.save()
+
+            messages.success(request, "Mission content saved successfully")
+            return redirect('about')
+    context = {'mission': mission, 'form': form}
+    return render(request, 'base/missionform.html', context)
+
+# edit mission section
+def editMission(request, pk):
+    mission = Mission.objects.get(id=pk)
+    form = MissionForm(instance=mission)
+    if request.method == 'POST':
+        form = MissionForm(request.POST, request.FILES, instance=mission)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, "Mission content updated successfully")
+            return redirect('about')
+    context = {'form': form}
+    return render(request, 'base/missionform.html', context)
+
+
+# delete mission section
+def deleteMission(request,pk):
+    mission = Mission.objects.get(id=pk)
+    if request.method == 'POST':
+        mission.delete()
+        
+        messages.success(request, 'Mission content deleted successfully')
+        return redirect('about')
+    context = {'obj': mission}
+    return render(request, 'delete.html', context)
+
+
+# add message from section
+def addMessageFrom(request):
+    message = MessageFrom.objects.all()
+    form = MessageFromForm()
+    if request.method == 'POST':
+        form = MessageFromForm(request.POST, request.FILES)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.save()
+
+            messages.success(request, "Message From content saved successfully")
+            return redirect('about')
+    context = {'message': message, 'form': form}
+    return render(request, 'base/messagefromform.html', context)
+
+# edit message from section
+def editMessageFrom(request, pk):
+    message = MessageFrom.objects.get(id=pk)
+    form = MessageFromForm(instance=message)
+    if request.method == 'POST':
+        form = MessageFromForm(request.POST, request.FILES, instance=message)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, "Message From content updated successfully")
+            return redirect('about')
+    context = {'form': form}
+    return render(request, 'base/messagefromform.html', context)
+
+
+# delete message from section
+def deleteMessageFrom(request,pk):
+    message = MessageFrom.objects.get(id=pk)
+    if request.method == 'POST':
+        message.delete()
+        
+        messages.success(request, 'Message From content deleted successfully')
+        return redirect('home')
+    context = {'obj': message}
+    return render(request, 'delete.html', context)
+
+
 # blogs page
 def blogs(request):
     blogs = Blog.objects.all()
