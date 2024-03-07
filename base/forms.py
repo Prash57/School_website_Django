033ab055a05c3f41a6e8df4989abba5ef3ea1982
Prashant Adhikari <aprashant057@gmail.com>
2024-03-07 20:31:23 +0545

    Layout redesigned for forms

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,24 @@
 from .models import *
 from django import forms
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'email', 'username', 'password1', 'password2']
+        labels = {
+            'first_name': 'Name',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control border-1'})
+
 
 class SchoolSetupForm(forms.ModelForm):
     class Meta:
@@ -116,6 +134,11 @@ class ContactForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+                field.widget.attrs.update({'class': 'form-control border-1'})
+
+
 
 class HomeContentForm(forms.ModelForm):
     class Meta:
